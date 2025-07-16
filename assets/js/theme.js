57217fb2831ab18259a92a4d3f9b2e2eb92fabad
yongjin <dyyjkd@kaist.ac.kr>
2025-07-16 22:48:49 +0900

    fix background

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,17 +1,15 @@
 // Has to be in the head tag, otherwise a flicker effect will occur.
 let initTheme = (theme) => {
   if (theme == null) {
-    const userPref = window.matchMedia;
-    if (userPref && userPref('(prefers-color-scheme: dark)').matches) {
-        theme = 'dark';
-    }
+    // Default to light theme instead of following system preference
+    theme = "light";
   }
 
-  if (theme)  {
-    document.documentElement.setAttribute('data-theme', theme)
+  if (theme) {
+    document.documentElement.setAttribute("data-theme", theme);
   }
 
   localStorage.setItem("theme", theme);
-}
+};
 
 initTheme(localStorage.getItem("theme"));
